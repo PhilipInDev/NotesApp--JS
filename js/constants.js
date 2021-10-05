f60711c9ba4ff45bb555df10c377aeb650026267
Felipe Barsaga Avila <philipatdev@gmail.com>
2021-10-05 21:16:58 +0300

    Adding basic constants

---
+++ js/constants.js
@@ -0,0 +1,16 @@
+export const categories = {
+    task: 'Task',
+    randomThought: 'Random Thought',
+    idea: 'Idea'
+}
+export const noteShape = {
+    id: null,
+    isActive: null,
+    name: '',
+    timeOfCreating: '',
+    category: '',
+    content: '',
+    dates: ''
+}
+export const columnNames = ['Name', 'Created', 'Category', 'Content', 'Dates'];
+export const columnNamesStatistic = ['Note Category', 'Active', 'Archived'];

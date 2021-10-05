3bcf868581ec51c3c1b9187bb79c985bec7a10a8
Felipe Barsaga Avila <philipatdev@gmail.com>
2021-10-05 21:17:52 +0300

    Creating basic store

---
+++ js/store.js
@@ -0,0 +1,13 @@
+import {categories} from "./constants";
+
+export const notes = [
+    {
+        id: 1,
+        isActive: true,
+        name: 'Shopping List',
+        timeOfCreating: '20 April, 2021',
+        category: categories.task,
+        content: 'Some text some text some text some text, ssssssssssssss',
+        dates: ''
+    }
+]
